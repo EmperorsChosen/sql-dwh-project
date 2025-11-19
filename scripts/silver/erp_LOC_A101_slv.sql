1a42a3faf770db51ce13125f99ea352d9880058d
Mikhail Renskiy <71692804+EmperorsChosen@users.noreply.github.com>
2025-11-19 17:37:02 +0200

    Create erp_LOC_A101_slv.sql

---
+++ scripts/silver/erp_LOC_A101_slv.sql
@@ -0,0 +1,16 @@
+o silver.erp_LOC_A101 (
+ cid,
+ cntry
+)
+select 
+ REPLACE(cid, '-', ''),
+ CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+      WHEN TRIM(cntry) IN ('US','USA') THEN 'United States'
+      WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+      ELSE TRIM(cntry)
+    END AS cntry
+from bronze.erp_LOC_A101
+
+
+
+select distinct cntry from bronze.erp_LOC_A101
